7631a5336459daf48489b3ba457eeaf984d3dac4
fbronchaly <fbronchaly@gmail.com>
2020-04-07 07:11:00 +0200

    4 files modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ElectricosComponent } from './electricos/electricos.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
+import { PasaDatosComponent } from './pasa-datos/pasa-datos.component';
 
 
 const APP_ROUTES: Routes = [
   { path: 'electricos', component: ElectricosComponent },
   { path: 'presentacion', component: PresentacionComponent },
+  { path: 'pasadatos', component: PasaDatosComponent },
  
   { path: '**', pathMatch: 'full', redirectTo: 'electricos' }
 ];
